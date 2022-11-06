c162cc4611a9fba6ac7c0bc8f2146a57995dc98e
Arnau Diaz <arnau.diaz@adevinta.com>
2022-11-06 19:36:05 +0100

    undo handler folder

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -1,5 +1,5 @@
 use env_logger::Env;
-use rust_cli_api::handlers::{shortener, state};
+use rust_cli_api::handler::{redirect, shortener, AppState};
 
 use std::{collections::HashMap, sync::Mutex};
 
@@ -7,7 +7,7 @@ use actix_web::{middleware::Logger, web, App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let state = web::Data::new(state::AppState {
+    let state = web::Data::new(AppState {
         counter: Mutex::new(0),
         map: Mutex::new(HashMap::new()),
     });
@@ -18,8 +18,8 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(Logger::default())
             .app_data(state.clone())
-            .service(shortener::handler)
-            .service(shortener::redirect)
+            .service(shortener)
+            .service(redirect)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- src/handlers/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod shortener;
-pub mod state;
